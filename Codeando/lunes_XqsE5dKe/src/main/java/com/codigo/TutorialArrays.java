e59841289423dcc6e138e5885cc4315cd8df58ea
Eduardo Ramirez <eduardoramirez@MacBook-Air-de-Eduardo.local>
2024-06-10 23:48:46 -0500

    update

---
+++ Codeando/lunes_XqsE5dKe/src/main/java/com/codigo/TutorialArrays.java
@@ -0,0 +1,10 @@
+package com.codigo;
+
+public class TutorialArrays {
+    public static void main(String[] args) {
+        int[] valores = new int[5];
+        int[] valores2 = {3,2,55,12,9};
+
+    }
+
+}
