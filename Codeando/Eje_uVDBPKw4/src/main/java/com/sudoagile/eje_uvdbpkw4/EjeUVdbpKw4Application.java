7b6ff4a0d05ec204c46a3d121eaffb0a50bc1d4c
Eduardo Ramirez De Lama <eramirez4>
2025-02-03 20:15:19 -0500

    2025

---
+++ Codeando/Eje_uVDBPKw4/src/main/java/com/sudoagile/eje_uvdbpkw4/EjeUVdbpKw4Application.java
@@ -0,0 +1,13 @@
+package com.sudoagile.eje_uvdbpkw4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjeUVdbpKw4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjeUVdbpKw4Application.class, args);
+    }
+
+}
---
+++ Codeando/Eje_uVDBPKw4/src/test/java/com/sudoagile/eje_uvdbpkw4/EjeUVdbpKw4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudoagile.eje_uvdbpkw4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjeUVdbpKw4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
